76283ece08d12054c4a0a2ff358863e5c185bf3b
pro <pro@prodeMacBook-Pro-2.local>
2023-06-19 01:01:43 +0600

    transfer balance for sender and reciver

--- bank.py
+++ bank.py
@@ -30,3 +30,14 @@ class User:
             self.transaction_history.append(f'Withdrew amount: {amount}')
         else:
             print("Insufficient funds! So please keep dollar in your own account")
+
+    def transfer(self, recipient, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+            recipient.balance += amount
+            self.transaction_history.append(
+                f'Transferred {amount} to account {recipient.account_number}')
+            recipient.transaction_history.append(
+                f'Received {amount} from account {self.account_number}')
+        else:
+            print("Insufficient funds!")
